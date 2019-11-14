4f8056409d4528022560410c2248986f6ab3853d
alexhprime <haragas6@gmail.com>
2019-11-14 20:55:56 +0200

    homework

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #define MAX_FOOD_NAME 100
 #define MAX_SPECIFIC_FOOD_NAME 100
 #define MAX_DRINK_NAME 100
-void readmenuitem(char *thing);
+void readmenuitem(char *menuitem);
 int main() {
     int nofood;
     printf("Please input number of food types: \n>");
@@ -13,7 +13,6 @@ int main() {
     printf("Please input food types (each on a new line, may contain spaces):\n");
     for(int i=0;i<nofood;i++)
     {
-        printf(">");
         food[i]=(char*)malloc(MAX_FOOD_NAME* sizeof(char));
         scanf("%s", food[i]);
     }
@@ -22,36 +21,34 @@ int main() {
     double ** foodprice=(double **)malloc(nofood* sizeof(double *));
     for(int i=0;i<nofood;i++)
     {
-        printf("Please input no of specific food for food %c%s%c: \n>",'"', food[i],'"');
+        printf("Please input no of specific food for food %s: \n", food[i]);
         scanf("%d",&nospecificfood[i]);
         getchar();
         specificfood[i]=(char**)malloc(nospecificfood[i]* sizeof(char*));
         foodprice[i]=(double *)malloc(nospecificfood[i]* sizeof(double));
-        printf("Please input %c%s%c specific foods & prices: each line in the format <specific food> (price): \n",'"', food[i],'"');
+        printf("Please input %s specific foods & prices: each line in the format <specific food> (price): \n",food[i]);
         for(int j=0;j<nospecificfood[i];j++)
         {
-            printf(">");
             specificfood[i][j]=(char *)malloc(MAX_SPECIFIC_FOOD_NAME* sizeof(char));
             readmenuitem(specificfood[i][j]);
             scanf("%lf",&foodprice[i][j]);
-            getchar();
+            getchar();//reads the ')' after the number
             getchar();
         }
     }
     int nodrink;
-    printf("Please input no of drinks:\n>");
+    printf("Please input no of drinks:\n");
     scanf("%d",&nodrink);
     getchar();
     char** drink=(char**)malloc(nodrink* sizeof(char*));
     double * drinkprice=(double *)malloc(nodrink* sizeof(double));
-    printf("Please input the drinks: eac line in the format <drink> (price):\n");
+    printf("Please input the drinks: each line in the format <drink> (price):\n");
     for(int i=0;i<nodrink;i++)
     {
-        printf(">");
         drink[i]=(char*)malloc(MAX_DRINK_NAME* sizeof(char));
         readmenuitem(drink[i]);
         scanf("%lf", &drinkprice[i]);
-        getchar();
+        getchar();//reads the ')' after the number
         getchar();
     }
     printf("The food data is:");
@@ -93,14 +90,14 @@ int main() {
     free(food);
     return 0;
 }
-void readmenuitem(char * thing) {
+void readmenuitem(char * menuitem) {
     char c=getchar();
     int i=0;
     while(c!='(')
     {
-        thing[i]=c;
+        menuitem[i]=c;
         c=getchar();
         i++;
     }
-    thing[i-1]='\0';
+    menuitem[i - 1]='\0';
 }
